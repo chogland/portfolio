e6f318660ba2f954faf2b7b096462aa5dc191283
HoglandC <hoglandc@stifel.com>
2020-03-03 09:58:24 -0600

    lint errors - minor package change for convenience

--- src/app/mock-portfolioitems.ts
+++ src/app/mock-portfolioitems.ts
@@ -1,13 +1,14 @@
 import { item } from './item';
 
 export const ITEMS: item[] = [
-  { id: 1, name: 'Website 1', description: 'Website 1 description', type: 'Web', image: "../assets/images/web/portfolio_thumbs_ff1.jpg" },
-  { id: 2, name: 'Website 2', description: 'Website 2 description', type: 'Web' , image: "../assets/images/web/portfolio_thumbs_ff2.jpg" },
-  { id: 3, name: 'Website 3', description: 'Website 3 description', type: 'Web', image: "../assets/images/web/portfolio_thumbs_irepBE.jpg"  },
-  { id: 4, name: 'Buz', description: 'Website 1 description', type: 'Music', image: "../assets/images/music/buz.jpg" },
-  { id: 5, name: 'We Are Warm', description: 'Drums: Recording/Live', type: 'Music', image: "../assets/images/music/waw.PNG"  },
-  { id: 6, name: 'Polebridge Electric Company', description: 'Drums: Recording/Live', type: 'Music', image: "../assets/images/music/pet.PNG"  },
-  { id: 7, name: 'Northport', description: 'Drums: Live', type: 'Music', image: "../assets/images/web/portfolio_thumbs_ff1.jpg"  },
-  { id: 8, name: 'El Monstero', description: 'Technician: Keyboards', type: 'Music', image: "../assets/images/web/portfolio_thumbs_ff1.jpg"  },
-
+  { id: 1, name: 'Website 1', description: 'Website 1 description', type: 'Web', image: '../assets/images/web/portfolio_thumbs_ff1.jpg'},
+  { id: 2, name: 'Website 2', description: 'Website 2 description', type: 'Web' , image: '../assets/images/web/portfolio_thumbs_ff2.jpg'},
+  { id: 3, name: 'Website 3', description: 'Website 3 description', type: 'Web', image: '../assets/images/web/portfolio_thumbs_irepBE.jpg'},
+  { id: 4, name: 'Buz', description: 'Website 1 description', type: 'Music', image: '../assets/images/music/buz.jpg' },
+  { id: 5, name: 'We Are Warm', description: 'Drums: Recording/Live', type: 'Music', image: '../assets/images/music/waw.PNG'},
+  { id: 6, name: 'Polebridge Electric Company', description: 'Drums: Recording/Live', type: 'Music',
+  image: '../assets/images/music/pet.PNG'},
+  { id: 7, name: 'Northport', description: 'Drums: Live', type: 'Music', image: '../assets/images/web/portfolio_thumbs_ff1.jpg'},
+  { id: 8, name: 'El Monstero', description: 'Technician: Keyboards', type: 'Music',
+  image: '../assets/images/web/portfolio_thumbs_ff1.jpg'},
 ];
