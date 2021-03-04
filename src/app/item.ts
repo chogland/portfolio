d5bdd2578a5b24ebee928fc680d4a7ee625e83cf
Charlie Hogland <charliehogland@gmail.com>
2021-03-04 15:48:46 -0600

    upgrade issues resolved

--- src/app/item.ts
+++ src/app/item.ts
@@ -1,3 +1,5 @@
+import { Injectable } from "@angular/core";
+@Injectable()
 export class item {
     id: number;
     name: string;
--- src/app/portfolio-item-details/portfolio-item-details.component.spec.ts
+++ src/app/portfolio-item-details/portfolio-item-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PortfolioItemDetailsComponent } from './portfolio-item-details.component';
 
@@ -6,7 +6,7 @@ describe('PortfolioItemDetailsComponent', () => {
   let component: PortfolioItemDetailsComponent;
   let fixture: ComponentFixture<PortfolioItemDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PortfolioItemDetailsComponent ]
     })
